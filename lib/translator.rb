e167575ff78c99560a6b9b349a1bef397e4c6b57
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-14 23:52:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,8 @@ def get_japanese_emoticon(file_path, emoticon)
   results = load_library(file_path)['get_emoticon'][emoticon]
   if results
     return results
+  else
+    return "Sorry, that emoticon was not found"
   end
 end
 
