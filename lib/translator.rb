78cc8ef36a17508284df284415164bafb0879b43
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-14 23:51:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,6 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  binding.pry
   results = load_library(file_path)['get_emoticon'][emoticon]
   if results
     return results
