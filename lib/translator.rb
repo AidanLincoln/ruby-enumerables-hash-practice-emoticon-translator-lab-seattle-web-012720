0afe9607c59fa7ca0bc72420408662593560508d
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-14 23:42:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,23 @@
-# require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+def load_library(file_path)
+  nds = YAML.load_file(file_path)
+  new_nds = {}
+  new_nds['get_meaning'] = {}
+  new_nds['get_emoticon'] = {}
+  nds.each do |meaning, array|
+    new_nds['get_meaning'][array[1]] = meaning
+    new_nds['get_emoticon'][array[0]] = array[1]
+  end
+  
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  load_library(file_path)
+  new_nds['get_emoticon'].each do |english_emote, japanese_emote|
+    if emoticon == english_emote
+      return japanese_emote
+    end
+  end
 end
 
 def get_english_meaning
